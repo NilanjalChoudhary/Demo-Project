d2d2dd32e4a2393d54b47becbeee6b5ff73d1846
Nilanjal <nilanjal.choudhary@preciousinfosystem.com>
2023-07-25 15:12:46 +0530

    saving changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_23_141143) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_090903) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -109,6 +109,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_23_141143) do
     t.integer "year_of_experience"
     t.integer "role"
     t.string "type"
+    t.boolean "confirm_by_admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
