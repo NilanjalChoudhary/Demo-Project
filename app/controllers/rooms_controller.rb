99c9afaac2192c4051745506d621e6fbd85301e2
Nilanjal <nilanjal.choudhary@preciousinfosystem.com>
2023-07-28 21:39:54 -0700

    Added Chat funtionality

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,29 +1,35 @@
 class RoomsController < ApplicationController
+
+  before_action :authenticate_user!
+
   def index
+    @room = Room.new
     @current_user = current_user
     @rooms = Room.public_rooms
+    # @single_room = Room.find(params[:id])
     @users = User.all_except(@current_user)
   end
 
   def show
     @current_user = current_user
     @single_room = Room.find(params[:id])
-    @rooms = Room.public_rooms
-    @users = User.all_except(@current_user)
+    
     @room = Room.new
-  
-    @messages = @single_room.messages
-
+    @rooms = Room.public_rooms
+    
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :desc)
+     
+    @users = User.all_except(@current_user)
     render "index"
   end
 
-  def new
-    @room = Room.new
-  end
+  # def new
+  #   @room = Room.new
+  # end
   
   
   def create
     @room = Room.create(name: params["room"]["name"])
   end
-
 end
